1a158a1fce5a8924aa759ca0a687dc6511621752
AHAman0 <aniqayyubi@gmail.com>
2025-08-18 21:09:09 +0700

    Checking
    Contribute
    no I do not mean last update I mean newest update.

--- application/views/Main/Body.php
+++ application/views/Main/Body.php
@@ -2,6 +2,7 @@
     <div class="container mt-2">
         <div class="justify-content-center">
             <h1>Welcome! My friends</h1>
+            <h1>This is a Financially corrected automatically Forex. Please be awaited</h1>
         </div>
     </div>
 </body>
--- application/views/Main/index.php
+++ application/views/Main/index.php
@@ -34,18 +34,6 @@
                         </li>
                     </ul>
                 </div>
-                <div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
-  <div class="bg-dark p-4">
-    <h5 class="text-body-emphasis h4">Collapsed content</h5>
-    <span class="text-body-secondary">Toggleable via the navbar brand.</span>
-  </div>
-</div>
-<nav class="navbar navbar-dark bg-dark">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  </div>
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
                     <ul class="nav nav-pills">
                         <li class="nav-item dropdown">
